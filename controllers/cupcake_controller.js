91db64a5ad9003e90f121ea9b3753340b5a9742f
liz-rondone <liz.rondone@northwestern.edu>
2017-08-15 13:13:52 -0500

    tuesday one

---
+++ controllers/cupcake_controller.js
@@ -0,0 +1,55 @@
+var express = require('express');
+
+var router = express.Router();
+
+var cupcake = require("../models/cupcake.js");
+
+//create all routes and set up logic within those routes where required
+
+// select
+router.get('/', function(req, res) {
+    cupcake.all(function(data) {
+        var hbsObject = {
+            cupcakes: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+// insert
+router.post('/', function(req, res) {
+    cupcake.create([
+        // name or cupcake_name?
+        "cupcake_name", "devoured"
+    ], [
+        // name or cupcake_name?
+        req.body.name, req.body.devoured
+    ], function() {
+        res.redirect('/');
+    });
+});
+
+// update
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    cupcake.update({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/");
+    });
+});
+
+// delete?
+// router.delete("/:id", function(req, res){
+//     var condition = "id = " + req.params.id;
+
+//     cat.delete(condition, function() {
+//         res.redirect('/');
+//     });
+// });
+
+module.exports = router;
---
+++ models/cupcake.js
@@ -0,0 +1,28 @@
+var orm = require('../config/orm.js');
+
+var cupcake = {
+    all: function(cb) {
+        orm.all('cupcakes', function(res) {
+            cb(res);
+        });
+    },
+
+    // the variables cols and vals are arrays
+    create: function(cols, vals, cb) {
+        orm.create('cupcakes', cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update('cupcakes', objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+    // delete: function(condition, cb) {
+    //     orm.delete('cupcakes', condition, function(res) {
+    //         cb(res);
+    //     });
+    // }
+};
+
+module.exports = cat;
